e9e30c1eadabf825155df1439946bcd63240222f
Sarah Wells <sarah@Sarahs-MacBook-Air.local>
2014-04-26 16:47:57 +0100

    First cut - does nothing yet

---
+++ src/main/java/com/github/sjwells/dropwizardquickstart/DropwizardQuickstartApplication.java
@@ -0,0 +1,22 @@
+package com.github.sjwells.dropwizardquickstart;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class DropwizardQuickstartApplication extends Application<DropwizardQuickstartConfiguration> {
+
+	@Override
+	public void initialize(Bootstrap<DropwizardQuickstartConfiguration> configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run(DropwizardQuickstartConfiguration configuration, Environment environment)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/github/sjwells/dropwizardquickstart/DropwizardQuickstartConfiguration.java
@@ -0,0 +1,7 @@
+package com.github.sjwells.dropwizardquickstart;
+
+import io.dropwizard.Configuration;
+
+public class DropwizardQuickstartConfiguration extends Configuration {
+
+}
